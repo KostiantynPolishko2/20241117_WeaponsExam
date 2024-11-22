b966389d87b2c8cbc1b9ab452ce9aa099fb2c577
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-11-22 21:44:24 +0200

    release v1.0.0 ClientPageServer (#3)
    * Create WeaponsCards.sln
    
    * 1 created HttpGet to get responce as string from out server (Azure adminpage-server) via HttpClient.
    
    * 2 managed using Cancaletion token and method handleException
    
    * 3 creating of instance HttpRequestMessage moved to builder. Service.AddTransient<HttpReqeustMessage> in Program.cs
    
    * 4 done class HandlerException and injected to middlaware
    
    * 5 done publish to Azure Web Api service

---
+++ ClientPageServer.PL/Controllers/WeaponsCardsController.cs
@@ -0,0 +1,49 @@
+﻿using ClientPageServer.PL.DTO;
+using ClientPageServer.PL.Infrastructures;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ClientPageServer.PL.Controllers
+{
+    [EnableCors("AllowAll")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeaponsCardsController : ControllerBase
+    {
+        private readonly ILogger<WeaponsCardsController> logger;
+        private readonly HttpRequestMessage httpRequestMsg;
+        private CancellationTokenSource cts = null!;
+        private readonly int timeRequest = 10;
+
+        public WeaponsCardsController(ILogger<WeaponsCardsController> logger, HttpRequestMessage httpRequestMsg)
+        {
+            this.httpRequestMsg = httpRequestMsg;
+            this.logger = logger;
+            setToken();
+        }
+        private void setToken()
+        {
+            this.cts = new CancellationTokenSource();
+            this.cts.CancelAfter(TimeSpan.FromSeconds(this.timeRequest));
+        }
+
+        [HttpGet("weapons-cards", Name = "GetWeaponsCards")]
+        public async Task<ActionResult<IEnumerable<WeaponsCardDto>>> GetWeaponsCards()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage responseMessage = client.Send(httpRequestMsg, this.cts.Token);
+
+                if (responseMessage.StatusCode == HttpStatusCode.OK)
+                {
+                    return Ok(await responseMessage.Content.ReadFromJsonAsync<List<WeaponsCardDto>>(this.cts.Token));
+                }
+                else
+                {
+                    throw new WeaponsException("Error! failed HttpClient.Send(), status code != 200", $"{HttpStatusCode.OK}");
+                }
+            }
+        }
+    }
+}
