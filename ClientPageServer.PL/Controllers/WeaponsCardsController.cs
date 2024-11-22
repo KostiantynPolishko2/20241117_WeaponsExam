cb215b3a45903dc4898d9df830be1dbea4d82848
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-11-22 15:58:00 +0200

    1 created HttpGet to get responce as string from out server (Azure adminpage-server) via HttpClient.

---
+++ ClientPageServer.PL/Controllers/WeaponsCardsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ClientPageServer.PL.Controllers
+{
+    [EnableCors("AllowAll")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeaponsCardsController : ControllerBase
+    {
+        private readonly ILogger<WeaponsCardsController> logger;
+
+        public WeaponsCardsController(ILogger<WeaponsCardsController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet("weapons-cards", Name = "GetWeaponsCards")]
+        public async Task<ActionResult<string>> GetWeaponsCards()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var uri = new Uri("https://adminpage-server.azurewebsites.net/api/WeaponsItems/client-models");
+                HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Get, uri);
+                httpRequest.Headers.Add("Accept", "application/json");
+
+                HttpResponseMessage responseMessage = client.Send(httpRequest);
+                var item = responseMessage.StatusCode;
+
+                if (responseMessage.StatusCode == HttpStatusCode.OK)
+                {
+                    return await responseMessage.Content.ReadAsStringAsync();
+                }
+
+                return "responce is empty";
+            }
+        }
+    }
+}
---
+++ ClientPageServer.PL/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ClientPageServer.PL", Version = "v1" });
+});
+
+//add CORS policy for permission treat request from other protocols and ports
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
